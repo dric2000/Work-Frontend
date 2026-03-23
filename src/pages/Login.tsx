58efe71c49a473be04c0fa4f11756a01a7066e56
dric2000 <ccedricbbleossi@gmail.com>
2026-03-23 17:45:54 +0100

    Premier commit

---
+++ src/pages/Login.tsx
@@ -0,0 +1,12 @@
+import { Button } from "../components/ui/button";
+
+const Login = () => {
+  return (
+    <div>
+      Login
+      <Button>OK</Button>
+    </div>
+  );
+};
+
+export default Login;
